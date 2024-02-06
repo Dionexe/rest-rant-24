2aca985372ebea9e7c07927c784d74e14ff20213
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-02-05 20:31:03 -0800

    Adding home page view

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello Rest-Rant World!')
-})
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+
 app.get('*', (req, res) => {
-    res.status(404).send ('<h1>404 Page Not Found</h1>')
+  res.send('404 page')
 })
 
+app.listen(process.env.PORT)
 
-app.listen(process.env.PORT, () => {
-  console.log('Server is Running on localhost')
-})
 
