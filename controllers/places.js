8efb7abc15c429723b18bcb1b1e00643a6171664
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-02-07 13:28:28 -0800

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -3,10 +3,26 @@ const router = require("express").Router();
 // router.get('/', (req, res) => {
 //     res.send('GET /places')
 // })
-router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
-  
+
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+router.post("/", (req, res) => {
+    console.log(req.body);
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = "http://placekitten.com/400/400";
+    }
+    if (!req.body.city) {
+      req.body.city = "Anytown";
+    }
+    if (!req.body.state) {
+      req.body.state = "USA";
+    }
+    places.push(req.body);
+    res.redirect("/places");
+  });
 
 // GET /places
 router.get("/", (req, res) => {
@@ -29,5 +45,7 @@ router.get("/", (req, res) => {
 
   res.render("places/index", { places });
 });
+
+
 
 module.exports = router;
