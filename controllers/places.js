f43c2479f004e5ae487b793b2481a3f5a5a6c16d
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-02-07 08:17:39 -0800

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -3,6 +3,10 @@ const router = require("express").Router();
 // router.get('/', (req, res) => {
 //     res.send('GET /places')
 // })
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+  
 
 // GET /places
 router.get("/", (req, res) => {
