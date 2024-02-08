35d1255895ce40f71d11716f52b3a615418d27e7
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-02-07 20:34:52 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -29,6 +29,19 @@ router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show')
+    }
+  })
+  
 
 
 module.exports = router;
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,22 +2,25 @@ const React = require("react");
 const Def = require("../Default");
 
 function index(data) {
-  let placesFormatted = data.places.map((place) => {
-    return (
-      <div key={place.name} className="col-sm-6">
-        <h2>{place.name}</h2>
-        <img src={place.pic} alt={place.name} />
-      </div>
-    );
-  });
-  return (
-    <Def>
-      <main>
-        <h1>Places to Rant or Rave About</h1>
-        <div className="row">{placesFormatted}</div>
-      </main>
-    </Def>
-  );
+    let placesFormatted = data.places.map((place, index) => {
+        return (
+          <div className="col-sm-6">
+            <h2>
+              <a href={`/places/${index}`} >
+                {place.name}
+              </a>
+            </h2>
+            <p className="text-center">
+              {place.cuisines}
+            </p>
+            <img src={place.pic} alt={place.name} />
+            <p className="text-center">
+              Located in {place.city}, {place.state}
+            </p>
+          </div>
+        )
+      })
+      
 }
 
 module.exports = index;
